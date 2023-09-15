c18b1c6b6e1cdb8cf53768873a75c545b597b91c
Crowdbotics <engineering@crowdbotics.com>
2023-09-15 18:37:49 +0000

    Update store.

--- store/softpoetryAPI/api.js
+++ store/softpoetryAPI/api.js
@@ -1,72 +1,55 @@
-import axios from "axios";
+import axios from "axios"
 const softpoetryAPI = axios.create({
   baseURL: "https://xtend-43171.botics.co",
-  headers: {
-    Accept: "application/json",
-    "Content-Type": "application/json"
-  }
-});
-
+  headers: { Accept: "application/json", "Content-Type": "application/json" }
+})
 function api_docs_schema_retrieve(payload) {
   return softpoetryAPI.get(`/api-docs/schema/`, {
-    params: {
-      lang: payload.lang
-    }
-  });
+    params: { lang: payload.lang }
+  })
 }
-
 function api_v1_login_create(payload) {
-  return softpoetryAPI.post(`/api/v1/login/`, payload.data);
+  return softpoetryAPI.post(`/api/v1/login/`, payload.data)
 }
-
 function api_v1_signup_create(payload) {
-  return softpoetryAPI.post(`/api/v1/signup/`, payload.data);
+  return softpoetryAPI.post(`/api/v1/signup/`, payload.data)
 }
-
 function rest_auth_login_create(payload) {
-  return softpoetryAPI.post(`/rest-auth/login/`, payload.data);
+  return softpoetryAPI.post(`/rest-auth/login/`, payload.data)
 }
-
 function rest_auth_logout_retrieve(payload) {
-  return softpoetryAPI.get(`/rest-auth/logout/`);
+  return softpoetryAPI.get(`/rest-auth/logout/`)
 }
-
 function rest_auth_logout_create(payload) {
-  return softpoetryAPI.post(`/rest-auth/logout/`);
+  return softpoetryAPI.post(`/rest-auth/logout/`)
 }
-
 function rest_auth_password_change_create(payload) {
-  return softpoetryAPI.post(`/rest-auth/password/change/`, payload.data);
+  return softpoetryAPI.post(`/rest-auth/password/change/`, payload.data)
 }
-
 function rest_auth_password_reset_create(payload) {
-  return softpoetryAPI.post(`/rest-auth/password/reset/`, payload.data);
+  return softpoetryAPI.post(`/rest-auth/password/reset/`, payload.data)
 }
-
 function rest_auth_password_reset_confirm_create(payload) {
-  return softpoetryAPI.post(`/rest-auth/password/reset/confirm/`, payload.data);
+  return softpoetryAPI.post(`/rest-auth/password/reset/confirm/`, payload.data)
 }
-
 function rest_auth_registration_create(payload) {
-  return softpoetryAPI.post(`/rest-auth/registration/`, payload.data);
+  return softpoetryAPI.post(`/rest-auth/registration/`, payload.data)
 }
-
 function rest_auth_registration_verify_email_create(payload) {
-  return softpoetryAPI.post(`/rest-auth/registration/verify-email/`, payload.data);
+  return softpoetryAPI.post(
+    `/rest-auth/registration/verify-email/`,
+    payload.data
+  )
 }
-
 function rest_auth_user_retrieve(payload) {
-  return softpoetryAPI.get(`/rest-auth/user/`);
+  return softpoetryAPI.get(`/rest-auth/user/`)
 }
-
 function rest_auth_user_update(payload) {
-  return softpoetryAPI.put(`/rest-auth/user/`, payload.data);
+  return softpoetryAPI.put(`/rest-auth/user/`, payload.data)
 }
-
 function rest_auth_user_partial_update(payload) {
-  return softpoetryAPI.patch(`/rest-auth/user/`, payload.data);
+  return softpoetryAPI.patch(`/rest-auth/user/`, payload.data)
 }
-
 export const apiService = {
   api_docs_schema_retrieve,
   api_v1_login_create,
@@ -82,4 +65,4 @@ export const apiService = {
   rest_auth_user_retrieve,
   rest_auth_user_update,
   rest_auth_user_partial_update
-};
+}
