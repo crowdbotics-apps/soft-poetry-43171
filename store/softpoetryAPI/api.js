d702ac1b07d7002521f7fe496ddc089c7047243d
Crowdbotics <engineering@crowdbotics.com>
2023-10-06 16:28:57 +0000

    Update store.

--- store/softpoetryAPI/api.js
+++ store/softpoetryAPI/api.js
@@ -9,13 +9,13 @@ function api_docs_schema_retrieve(payload) {
   })
 }
 function api_v1_login_create(payload) {
-  return softpoetryAPI.post(`/api/v1/login/`, payload.data)
+  return softpoetryAPI.post(`/api/v1/login/`, payload)
 }
 function api_v1_signup_create(payload) {
-  return softpoetryAPI.post(`/api/v1/signup/`, payload.data)
+  return softpoetryAPI.post(`/api/v1/signup/`, payload)
 }
 function rest_auth_login_create(payload) {
-  return softpoetryAPI.post(`/rest-auth/login/`, payload.data)
+  return softpoetryAPI.post(`/rest-auth/login/`, payload)
 }
 function rest_auth_logout_retrieve(payload) {
   return softpoetryAPI.get(`/rest-auth/logout/`)
@@ -24,31 +24,28 @@ function rest_auth_logout_create(payload) {
   return softpoetryAPI.post(`/rest-auth/logout/`)
 }
 function rest_auth_password_change_create(payload) {
-  return softpoetryAPI.post(`/rest-auth/password/change/`, payload.data)
+  return softpoetryAPI.post(`/rest-auth/password/change/`, payload)
 }
 function rest_auth_password_reset_create(payload) {
-  return softpoetryAPI.post(`/rest-auth/password/reset/`, payload.data)
+  return softpoetryAPI.post(`/rest-auth/password/reset/`, payload)
 }
 function rest_auth_password_reset_confirm_create(payload) {
-  return softpoetryAPI.post(`/rest-auth/password/reset/confirm/`, payload.data)
+  return softpoetryAPI.post(`/rest-auth/password/reset/confirm/`, payload)
 }
 function rest_auth_registration_create(payload) {
-  return softpoetryAPI.post(`/rest-auth/registration/`, payload.data)
+  return softpoetryAPI.post(`/rest-auth/registration/`, payload)
 }
 function rest_auth_registration_verify_email_create(payload) {
-  return softpoetryAPI.post(
-    `/rest-auth/registration/verify-email/`,
-    payload.data
-  )
+  return softpoetryAPI.post(`/rest-auth/registration/verify-email/`, payload)
 }
 function rest_auth_user_retrieve(payload) {
   return softpoetryAPI.get(`/rest-auth/user/`)
 }
 function rest_auth_user_update(payload) {
-  return softpoetryAPI.put(`/rest-auth/user/`, payload.data)
+  return softpoetryAPI.put(`/rest-auth/user/`, payload)
 }
 function rest_auth_user_partial_update(payload) {
-  return softpoetryAPI.patch(`/rest-auth/user/`, payload.data)
+  return softpoetryAPI.patch(`/rest-auth/user/`, payload)
 }
 export const apiService = {
   api_docs_schema_retrieve,
